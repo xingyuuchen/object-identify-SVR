1f46da8a8386ac44d3e9e69e3260d7b1a924523e
xingyuuchen <16602809965@163.com>
2021-01-19 13:25:35 +0800

    poll阻塞recv

--- socket/unix_socket.h
+++ socket/unix_socket.h
@@ -6,6 +6,7 @@
 #include <arpa/inet.h>
 #include "../autobuffer.h"
 #include "../log.h"
+#include "../time/timeutil.h"
 
 #include <vector>
 #include <poll.h>
@@ -21,14 +22,16 @@ class UnixSocket {
   public:
     
     static size_t BlockSocketReceive(SOCKET _socket, AutoBuffer &_recv_buff,
-                             size_t _buff_size, int _time_out_mills = 3000) {
-        if (_time_out_mills < -1) { _time_out_mills = -1; }
-        
+                             size_t _buff_size, int _timeout_mills = 3000) {
+        if (_timeout_mills < -1) { _timeout_mills = -1; }
         size_t available = _recv_buff.AvailableSize();
         if (available < _buff_size) {
             _recv_buff.AddCapacity(_buff_size - available);
         }
+        
         ssize_t nrecv = 0;
+        uint64_t start_time = GetCurrentTimeMillis();
+        uint64_t cost_time = 0;
     
         std::vector<pollfd> pollfds;
         pollfd fd;
@@ -36,19 +39,22 @@ class UnixSocket {
         fd.revents = 0;
         fd.events = POLLIN;
         pollfds.push_back(fd);
-//        while (true) {
+        
+        while (true) {
             for (pollfd fd : pollfds) {
                 fd.revents = 0;
             }
+    
             // poll函数跟select一样，可以处理多路复用。可以通过设置关注的描述符事件，灵活等待事件的到来。
-            int ret = poll(&pollfds[0], (nfds_t) pollfds.size(), _time_out_mills);
+            int poll_timeout = _timeout_mills > cost_time ? _timeout_mills - cost_time : 0;
+            int ret = poll(&pollfds[0], (nfds_t) pollfds.size(), poll_timeout);
+            
             if (ret < 0) {
                 int errno_ = errno;
                 Log("[BlockSocketReceive] poll errno: %d", errno_)
                 return -1;
             } else if (ret == 0) {
-                // 超时, 没有事件发生
-                Log("[BlockSocketReceive] poll timeout")
+                Log("[BlockSocketReceive] timeout, nrecv = %zd", nrecv)
                 return nrecv;
             } else {
                 Log("[BlockSocketReceive] poll ret: %d", ret)
@@ -57,26 +63,37 @@ class UnixSocket {
                         continue;
                     }
                     if (pollfds[i].revents & POLLIN) {
-                        nrecv = recv(_socket, _recv_buff.Ptr(_recv_buff.Length()), _buff_size, 0);
-                        if (nrecv > 0) {
+                        ssize_t n = recv(_socket, _recv_buff.Ptr(_recv_buff.Length()),
+                                         _buff_size - nrecv, 0);
+                        
+                        if (n > 0) {
                             _recv_buff.AddLength(nrecv);
-        
-                        } else if (nrecv == 0) {
+                            nrecv += n;
+                            if (nrecv >= _buff_size) {
+                                return nrecv;
+                            }
+                            
+                        } else if (n == 0) {
                             /*  If no messages are available to be
                                 received and the peer has performed an
                                 orderly shutdown, the value 0 is
                                 returned.
                              */
+                            Log("[BlockSocketReceive] n == 0, nrecv = %zd", nrecv)
                         } else {
-                            Log("[BlockSocketReceive] len = %zd", nrecv)
+                            Log("[BlockSocketReceive] n:%zd, nrecv = %zd", n, nrecv)
+                            return nrecv;
                         }
+                    } else if (pollfds[i].revents & POLLERR) {
+                        Log("[BlockSocketReceive] POLLERR, nrecv = %zd", nrecv)
+                        return nrecv;
                     } else {
-                        Log("[BlockSocketReceive] revents[%d]: %d", i, pollfds[i].revents)
+                        Log("[BlockSocketReceive] revents:%d", pollfds[i].revents)
                     }
                 }
             }
-//        }
-        return nrecv;
+            cost_time = GetCurrentTimeMillis() - start_time;
+        }
     }
     
 };
---
+++ time/timeutil.h
@@ -0,0 +1,10 @@
+#ifndef OI_SVR_TIMEUTIL_H
+#define OI_SVR_TIMEUTIL_H
+
+#include <stdint.h>
+
+
+uint64_t GetCurrentTimeMillis();
+
+
+#endif //OI_SVR_TIMEUTIL_H
