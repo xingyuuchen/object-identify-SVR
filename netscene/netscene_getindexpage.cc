48366177e00968fc9e179e55479e2d99dcaafb65
xingyuuchen <16602809965@163.com>
2021-02-18 18:58:59 +0800

    [feat] add visittime to index page

--- netscene/netscene_getindexpage.cc
+++ netscene/netscene_getindexpage.cc
@@ -12,7 +12,9 @@ int NetSceneGetIndexPage::GetType() { return kIndexPage; }
 NetSceneBase * NetSceneGetIndexPage::NewInstance() { return new NetSceneGetIndexPage(); }
 
 int NetSceneGetIndexPage::DoSceneImpl(const std::string &_in_buffer) {
-    std::string resp = "If you see this, the server is running normally.";
+    static int visit_times_since_last_boot_ = 0;
+    std::string resp = "If you see this, the server is running normally, "
+                       "visit times since last boot: " + std::to_string(++visit_times_since_last_boot_);
     CopyRespToSendBody(resp, resp.size());
     return 0;
 }
