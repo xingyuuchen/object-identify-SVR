c2bc3a200126b5bab181a241cdc0341b65385b14
xingyuuchen <16602809965@163.com>
2021-02-18 18:59:33 +0800

    [bugfix] delete unnecessary static

--- constantsprotocol.h
+++ constantsprotocol.h
@@ -4,9 +4,9 @@
 /**
  * all NetScene type declared here.
  */
-static const int kIndexPage = 0;
-static const int kNetSceneTypeQueryImg = 1;
-static const int kNetSceneTypeGetTrainProgress = 2;
+const int kIndexPage = 0;
+const int kNetSceneTypeQueryImg = 1;
+const int kNetSceneTypeGetTrainProgress = 2;
 
 
 #endif //OI_SVR_CONSTANTSPROTOCOL_H
