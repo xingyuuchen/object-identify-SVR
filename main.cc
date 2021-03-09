93b68b03cd8da3f43107a636cf391ee9c22c8e71
xingyuuchen <16602809965@163.com>
2021-03-09 18:54:24 +0800

    [bugfix] main

--- main.cc
+++ main.cc
@@ -1,12 +1,8 @@
-#include "utils/log.h"
+#include "log.h"
 #include "httpserver.h"
 
-#include <list>
 int main(int argc, char **argv) {
     LogI("main", "Launching SVR...");
-    
-    std::list<int> ints;
-    std::list<int>::iterator a = ints.begin();
     
     HttpServer::Instance().Run();
     
