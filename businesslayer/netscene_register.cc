9162dc8aaf2d51c24f26578cd4990328577ec180
xingyuuchen <16602809965@163.com>
2021-03-19 16:12:45 +0800

    [feat] DBItem类

--- businesslayer/netscene_register.cc
+++ businesslayer/netscene_register.cc
@@ -1,4 +1,5 @@
 #include "netscene_register.h"
+#include <utility>
 #include "netscenetypes.h"
 #include "log.h"
 #include "netsceneregister.pb.h"
@@ -17,7 +18,10 @@ NetSceneBase *NetSceneRegister::NewInstance() { return new NetSceneRegister(); }
 int NetSceneRegister::DoSceneImpl(const std::string &_in_buffer) {
     static std::mutex mutex_;
     int32_t id = -1;
-    int ret = Dao::Insert("insert into users(nickname, avatar_path) values (null, null)");
+    
+    RowUserInfo new_usr("'NULL'", "'NULL'");
+    int ret = Dao::Insert(new_usr);
+    
     if (ret > 0) {
         id = ret;
     } else {
@@ -37,3 +41,30 @@ int NetSceneRegister::DoSceneImpl(const std::string &_in_buffer) {
     return 0;
 }
 
+
+const char *RowUserInfo::table_ = "users";
+const char *RowUserInfo::field_name1_ = "nickname";
+const char *RowUserInfo::field_name2_ = "avatar_path";
+
+uint64_t RowUserInfo::GetUsrId() const { return usr_id_; }
+
+RowUserInfo::RowUserInfo(std::string _nickname, std::string _avatar_path)
+        : nickname_(std::move(_nickname))
+        , avatar_path_(std::move(_avatar_path))
+        , usr_id_(0) {
+    OnDataPrepared();
+}
+
+const char *RowUserInfo::table() const {
+    return table_;
+}
+
+void RowUserInfo::PopulateFieldList(std::vector<std::string> &_filed_list) const {
+    _filed_list.emplace_back(field_name1_);
+    _filed_list.emplace_back(field_name2_);
+}
+
+void RowUserInfo::PopulateValueList(std::vector<std::string> &_value_list) const {
+    _value_list.push_back(nickname_);
+    _value_list.push_back(avatar_path_);
+}
--- businesslayer/netscene_register.h
+++ businesslayer/netscene_register.h
@@ -1,6 +1,7 @@
 #ifndef OI_SVR_NETSCENE_REGISTER_H
 #define OI_SVR_NETSCENE_REGISTER_H
 #include "netscenebase.h"
+#include "dbitem.h"
 
 
 class NetSceneRegister : public NetSceneBase {
@@ -15,6 +16,29 @@ class NetSceneRegister : public NetSceneBase {
 
   private:
   
+};
+
+
+class RowUserInfo : public DBItem {
+  public:
+
+    explicit RowUserInfo(std::string _nickname, std::string _avatar_path);
+
+    const char *table() const override;
+    
+    void PopulateFieldList(std::vector<std::string> &_filed_list) const override;
+    
+    void PopulateValueList(std::vector<std::string> &_value_list) const override;
+    
+    uint64_t GetUsrId() const;
+
+  private:
+    static const char * table_;
+    static const char * field_name1_;
+    static const char * field_name2_;
+    uint64_t            usr_id_;
+    std::string         nickname_;
+    std::string         avatar_path_;
 };
 
 
